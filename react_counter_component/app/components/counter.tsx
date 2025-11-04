23a9b277190162ba5eb6e49dc90b13c1e715e32b
Chan Wai Lung <chan0629w@gmail.com>
2025-11-04 18:38:07 +0800

    Completion of React Counter Component

--- react_counter_component/app/components/counter.tsx
+++ react_counter_component/app/components/counter.tsx
@@ -1,14 +1,20 @@
 import React, {useState} from "react";
 import "./../css/App.css";
 
-
+// Counter Component
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  
-  return (
-    <div className="Counter-Container">
-        <h1 className="Counter">Counter: {count}</h1>
-        <button onClick={() => setCount(count + 1)} className="Button">Increment</button>
-    </div>
-  );
+    // State to hold the count value
+    // setCount = function to update the count value
+    // count = current value of the counter
+    const [count, setCount] = useState(0);
+    
+    return (
+        
+        <div className="Counter-Container">
+            {/* Display the current count value */}
+            <h1 className="Counter">Counter: {count}</h1>
+            {/* Button to increment the count value */}
+            <button onClick={() => setCount(count + 1)} className="Button">Increment</button>
+        </div>
+    );
 }
